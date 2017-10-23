19d98cd8cf15a62c16b47290b6ef046df32c26a2
Daniel Cazzulino <daniel@cazzulino.com>
2017-10-23 19:25:49 -0300

    Pretty URLs for DevDiv's TFS

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Linkinator
-Provides short nice links for DevDiv's TFS
+# Linkinator 2000
+
+Provides short nice links for DevDiv's TFS:
+
+
+| Original URL | Nice URL|
+| ------------ |-------------|
+| https://devdiv.visualstudio.com/DevDiv/_workitems/edit/[WORK_ITEM_ID] | http://work.devdiv.io/94612 |
+| https://devdiv.visualstudio.com/DevDiv/_wiki?pagePath=%2FSome%2FSubfolder%2FPage | http://wiki.devdiv.io/Some/Subfolder/Page |
---
+++ background.js
@@ -0,0 +1,38 @@
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // With a new rule ...
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          // That fires when a page's URL contains 'devdiv.visualstudio.com' ...
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'devdiv.visualstudio.com/DevDiv' },
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }
+      ]);
+    });
+  });
+
+// Called when the user clicks on the page action.
+chrome.pageAction.onClicked.addListener(function(tab) {
+    var shortUrl = tab.url.toString();
+    if (shortUrl.includes('https://devdiv.visualstudio.com/DevDiv/XamarinVS/_wiki?pagePath='))
+        shortUrl = 'http://wiki.devdiv.io' + decodeURIComponent(shortUrl.substring(64));
+        if (shortUrl.includes('https://devdiv.visualstudio.com/DevDiv/_wiki?pagePath='))
+        shortUrl = 'http://wiki.devdiv.io' + decodeURIComponent(shortUrl.substring(54));
+    if (shortUrl.includes('https://devdiv.visualstudio.com/DevDiv/_workitems/edit'))
+        shortUrl = 'http://work.devdiv.io/' + shortUrl.replace("https://devdiv.visualstudio.com/DevDiv/_workitems/edit/", "");
+    
+    var copyFrom = document.createElement('textarea');
+    copyFrom.textContent = shortUrl;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+  });
