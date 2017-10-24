0d589c2157cc71fdadbd90f341928139cdd92947
Daniel Cazzulino <daniel@cazzulino.com>
2017-10-24 11:09:39 -0300

    Split Chrome / WebExtensions (Edge+Firefox) extensions
    Since the icon requirements and the APIs are slightly different,
    this was easier.

---
+++ chrome/background.js
@@ -0,0 +1,37 @@
+function onClicked(tab) {
+  var shortUrl = tab.url.toString();
+  if (shortUrl.includes('_wiki?pagePath='))
+      shortUrl = 'http://wiki.devdiv.io/' + decodeURIComponent(shortUrl.substring(shortUrl.indexOf('_wiki?pagePath=') + 15));
+  if (shortUrl.includes('https://devdiv.visualstudio.com/DevDiv/_workitems/edit'))
+      shortUrl = 'http://work.devdiv.io/' + shortUrl.replace("https://devdiv.visualstudio.com/DevDiv/_workitems/edit/", "");
+  
+  var copyFrom = document.createElement('textarea');
+  copyFrom.textContent = shortUrl;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  body.removeChild(copyFrom);
+}
+
+chrome.pageAction.onClicked.addListener(onClicked);
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains 'devdiv.visualstudio.com' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'devdiv.visualstudio.com' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
